5d6d1d9cc08664f02c0abced2f8db03417270740
Anshuman Prasad Mahanta <95046825+AnshumanPM@users.noreply.github.com>
2022-11-23 06:52:30 +0530

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+# BotStatus
+Updates your bot status in the message, every two hours.
+
+# Variables.
+
+- `APP_ID` and `API_HASH` from [my.telegram.org](https://my.telegram.org).
+- `BOTS` - TG UserName of your bots separated by space.
+- `SESSION` - Telethon SessionString of the User to edit the message.
+- `CHANNEL_ID` - ID of your channel.
+- `MESSAGE_ID` - ID of the message to edit.
+
+Fill these in a `.env` file
+
+# Credits
+
+- [odysseusmax](https://github.com/odysseusmax/bug-free-broccoli)
+- [Telethon](https://github.com/LonamiWebs/Telethon)
---
+++ bot.py
@@ -0,0 +1,68 @@
+import pytz
+import logging
+import asyncio
+from decouple import config
+from datetime import datetime as dt
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.sessions import StringSession
+from telethon import TelegramClient
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.INFO)
+
+try:
+    appid = config("APP_ID")
+    apihash = config("API_HASH")
+    session = config("SESSION", default=None)
+    chnl_id = config("CHANNEL_ID", cast=int)
+    msg_id = config("MESSAGE_ID", cast=int)
+    botlist = config("BOTS")
+    bots = botlist.split()
+    session_name = str(session)
+    user_bot = TelegramClient(StringSession(session_name), appid, apihash)
+    print("Started")
+except Exception as e:
+    print(f"ERROR\n{str(e)}")
+
+async def Stats():
+    async with user_bot:
+        while True:
+            print("[INFO] starting to check uptime..")
+            await user_bot.edit_message(int(chnl_id), msg_id, "**📈 | Real-Time Bot Status**\n\n`Performing a periodic check...`")
+            c = 0
+            edit_text = "**📈 | Real-Time Bot Status**\n\n"
+            for bot in bots:
+                print(f"[INFO] checking @{bot}")
+                snt = await user_bot.send_message(bot, "/start")
+                await asyncio.sleep(10)
+
+                history = await user_bot(GetHistoryRequest(
+                    peer=bot,
+                    offset_id=0,
+                    offset_date=None,
+                    add_offset=0,
+                    limit=1,
+                    max_id=0,
+                    min_id=0,
+                    hash=0
+                ))
+                msg = history.messages[0].id
+                if snt.id == msg:
+                    print(f"@{bot} is down.")
+                    edit_text += f"🤖  @{bot}\n        └ **Down** ❌\n\n"
+                elif snt.id + 1 == msg:
+                    edit_text += f"🤖  @{bot}\n        └ **Up** ✅\n\n"
+                await user_bot.send_read_acknowledge(bot)
+                c += 1
+                await user_bot.edit_message(int(chnl_id), msg_id, edit_text)
+            k = pytz.timezone("Asia/Kolkata")
+            month = dt.now(k).strftime("%B")
+            day = dt.now(k).strftime("%d")
+            year =  dt.now(k).strftime("%Y")
+            t = dt.now(k).strftime("%H:%M:%S")
+            edit_text +=f"\n**✔️Last Checked:** \n`{t} - {day} {month} {year} [IST]`\n\n__♻️ Refreshes automatically__"
+            await user_bot.edit_message(int(chnl_id), msg_id, edit_text)
+            print(f"Checks since last restart - {c}")
+            print("Sleeping for 2 hours.")
+            await asyncio.sleep(2 * 60 * 60)
+
+user_bot.loop.run_until_complete(Stats())
